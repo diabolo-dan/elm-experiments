c0950074249cbb217399839b38234041ef8200f5
diabolo-dan <diabolo.dan@gmail.com>
2018-05-08 20:06:46 +0100

    Implement simple test for purpose of basic exploration

--- tests/Example.elm
+++ tests/Example.elm
@@ -7,4 +7,10 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    todo "Implement our first test. See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!"
+    describe "Test experiment"
+        [ describe "addition"
+            [ test "simplest thing" <|
+                \_ ->
+                    1 + 1 |> Expect.equal 2
+            ]
+        ]
